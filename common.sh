7faa5ed06847964eb0a6ddc199c817947936832f
Chris Jenkins <chrisj@redhat.com>
2014-12-23 15:29:46 +0000

    Update

--- common.sh
+++ common.sh
@@ -11,9 +11,9 @@ WORKSPACE_ERR=5
 SRPMBUILD_ERR=6
 MODBUILD_ERR=7
 PUSH_USER=jenkins
-SATELLITE=sat6-workshop.lcy.redhat.com
-REPO_ID=10
-PUPPET_REPO_ID=11
+SATELLITE=sat6.chrisj.com
+REPO_ID=85
+PUPPET_REPO_ID=84
 RSA_ID=/var/lib/jenkins/.ssh/id_rsa
 KNOWN_HOSTS=/var/lib/jenkins/.ssh/known_hosts
 TESTVM_HOSTGROUP="Test Servers"
@@ -22,7 +22,7 @@ TEST_ROOT=r3dhat00
 MODAUTHOR=acme
 YUM_REPO=/var/www/html/pub/soe-repo
 PUPPET_REPO=/var/www/html/pub/soe-puppet
-CV=7.0.0-ci
-ORG=Default_Organization
+CV=RHEL7-CII
+ORG=ChrisJ
 
 
